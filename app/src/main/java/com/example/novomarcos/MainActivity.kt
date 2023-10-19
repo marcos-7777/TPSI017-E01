60c65240ebb2930054ea01697ca6154091cab64a
MarcosAraujo <marcos.arq@protonmail.com>
2023-10-19 20:52:08 +0100

    Commit 2023-10-19_2
    Commit 2023-10-19_2

--- app/src/main/java/com/example/novomarcos/MainActivity.kt
+++ app/src/main/java/com/example/novomarcos/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.novomarcos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.novomarcos.ui.theme.NovoMarcosTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +37,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+
+     //Image(painter = painterResource(id = R.drawable.ciberseguranca_fccn), contentDescription = "", modifier = Modifier.size(48.dp))
+    }
+    Row {
+        Column {
+            Text(text = "Marcos Araujo")
+            Text(text = "2023/10/19")
+        }
+        Image(
+            painter = painterResource(id = R.drawable.ciberseguranca_fccn),
+            contentDescription = "",
+            modifier = Modifier.size(48.dp))
+
+
+    }
+ 
 }
 
 @Preview(showBackground = true)
